4c673fd6029987e426d02c7826b198c1fc785564
Artem Gusev <gusev2014russia@mail.ru>
2020-06-14 21:57:25 +0300

    updated package.json and added build

---
+++ build/precache-manifest.c94bea6c3eef28b305a93fe666aa3868.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981e1b62332c7c945e0767720f0bc031",
+    "url": "/flappy-bird/index.html"
+  },
+  {
+    "revision": "31b2df910331995f213f",
+    "url": "/flappy-bird/static/css/3.665f35cb.chunk.css"
+  },
+  {
+    "revision": "afcd17d138108b9088bb",
+    "url": "/flappy-bird/static/css/4.d38267b0.chunk.css"
+  },
+  {
+    "revision": "611013e65d7d390a2fa0",
+    "url": "/flappy-bird/static/css/5.b2def9f0.chunk.css"
+  },
+  {
+    "revision": "ff155c011fdc167206c5",
+    "url": "/flappy-bird/static/css/main.a9b272be.chunk.css"
+  },
+  {
+    "revision": "9cdc6801d3ede128ce7a",
+    "url": "/flappy-bird/static/js/2.7b16f4c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flappy-bird/static/js/2.7b16f4c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b2df910331995f213f",
+    "url": "/flappy-bird/static/js/3.85a01fd4.chunk.js"
+  },
+  {
+    "revision": "afcd17d138108b9088bb",
+    "url": "/flappy-bird/static/js/4.a33ac192.chunk.js"
+  },
+  {
+    "revision": "611013e65d7d390a2fa0",
+    "url": "/flappy-bird/static/js/5.3dffcf82.chunk.js"
+  },
+  {
+    "revision": "ff155c011fdc167206c5",
+    "url": "/flappy-bird/static/js/main.44cbb8a8.chunk.js"
+  },
+  {
+    "revision": "fc8db2bf2f0efa2c1cbe",
+    "url": "/flappy-bird/static/js/runtime-main.2ff9e0c9.js"
+  },
+  {
+    "revision": "f97dc14b469e7aba06579207a33b1946",
+    "url": "/flappy-bird/static/media/bg.f97dc14b.png"
+  },
+  {
+    "revision": "cab12c378520d317a8ec94585fa4af5f",
+    "url": "/flappy-bird/static/media/bird.cab12c37.png"
+  },
+  {
+    "revision": "6ecbd5373509a49868bb5c373f34d881",
+    "url": "/flappy-bird/static/media/fg.6ecbd537.png"
+  },
+  {
+    "revision": "115aeb7ab95ecaf6879d56b9b4d2185b",
+    "url": "/flappy-bird/static/media/fly.115aeb7a.mp3"
+  },
+  {
+    "revision": "0e65a6bea18614e93811fc5d6bca6bac",
+    "url": "/flappy-bird/static/media/logo.0e65a6be.png"
+  },
+  {
+    "revision": "4d9d45e3064b061515ee8ff1589ba139",
+    "url": "/flappy-bird/static/media/note.4d9d45e3.svg"
+  },
+  {
+    "revision": "72581d85a78135ed15773554f5cc4d32",
+    "url": "/flappy-bird/static/media/octocat.72581d85.svg"
+  },
+  {
+    "revision": "07cd2335e8acb92c775ef37c15a9016c",
+    "url": "/flappy-bird/static/media/pipeNorth.07cd2335.png"
+  },
+  {
+    "revision": "f791429212203924c69e2f4096681cea",
+    "url": "/flappy-bird/static/media/pipeSouth.f7914292.png"
+  },
+  {
+    "revision": "30b534d8c4c5e9617a87f8e96949a090",
+    "url": "/flappy-bird/static/media/score.30b534d8.mp3"
+  }
+]);
