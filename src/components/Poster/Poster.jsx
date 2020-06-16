16fab8025c77f862c0f0432b320b5008158fd011
Artem Gusev <gusev2014russia@mail.ru>
2020-06-17 00:33:54 +0300

    added lazy load for load logo

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -5,7 +5,7 @@ import classes from './Poster.module.css';
 const Poster = (props) => {
     return (
         <div className={classes.Poster}>
-            <img src={Logo} alt="Logo"/>
+            <img src={Logo} alt="Logo" loading="lazy"/>
         </div>
     )
 }
