202d2815606e39e38950d6b611e0346735096003
Artem Gusev <gusev2014russia@mail.ru>
2020-09-12 10:50:29 +0300

    rewrite List and Menu components, renames App component, removes asyncComponent hoc

--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import classes from './Menu.module.css';
 import {Route, Switch} from 'react-router-dom';
-import asyncComponent from '../../hoc/asyncComponent';
 import {withRouter} from 'react-router-dom';
+import List from './List/List';
 
-const asyncSceneLoad = asyncComponent(() => import('./Scene/Scene'));
-const asyncAboutLoad = asyncComponent(() => import('./About/About'));
-const asyncListLoad = asyncComponent(() => import('./List/List'));
+const Scene = React.lazy(() => import('./Scene/Scene'));
+const About = React.lazy(() => import('./About/About'));
 
 
 const Menu = (props) => {
+    const routes = (<Switch>
+        <Route path='/game' component={Scene}/>
+        <Route path='/author' component={About}/>
+        <Route path='/' component={List}/>
+    </Switch>);
+
     return (
         <div className={classes.Menu}>
             <button
                 onClick={() => props.history.goBack()}
-                hidden={props.history.location.pathname === '/' || props.history.location.pathname === '/flappy-bird' || props.history.location.pathname === '/flappy-bird/'}
-            >Back</button>
-            <Switch>
-                <Route path='/game' component={asyncSceneLoad}/>
-                <Route path='/author' component={asyncAboutLoad}/>
-                <Route path='/' component={asyncListLoad}/>
-            </Switch>
+                hidden={props.history.location.pathname === '/' ||
+                props.history.location.pathname === '/flappy-bird' ||
+                props.history.location.pathname === '/flappy-bird/'}
+            >Back
+            </button>
+            <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
             <div className={classes.Links}>
                 <a
                     href='https://github.com/CreativeRusBear/FlappyBird/blob/master/LICENSE'
