8275604b5d6296091fee8fdf9111a2899b8bbfe0
Artem Gusev <gusev2014russia@mail.ru>
2021-07-10 19:50:00 +0300

    Adds snapshot test for the Poster component

---
+++ src/components/Poster/Poster.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Poster from './Poster';
+
+it('Poster component renders correctly', () => {
+  const tree = renderer
+    .create(<Poster />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
