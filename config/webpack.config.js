1bfeefe84230f594445663119c45f910cb76ddae
Artem Gusev <gusev2014russia@mail.ru>
2022-01-09 22:48:10 +0300

    removes using `postcss-preset-env` package

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -90,12 +90,6 @@ module.exports = function(webpackEnv) {
           ident: 'postcss',
           plugins: () => [
             require('postcss-flexbugs-fixes'),
-            require('postcss-preset-env')({
-              autoprefixer: {
-                flexbox: 'no-2009',
-              },
-              stage: 3,
-            }),
             // Adds PostCSS Normalize as the reset css with default options,
             // so that it honors browserslist config in package.json
             // which in turn let's users customize the target behavior as per their needs.
