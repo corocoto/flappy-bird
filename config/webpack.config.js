77b68320da525eca58cba8dc6ffafbfac1e8e754
Artem Gusev <gusev2014russia@mail.ru>
2022-01-09 22:50:02 +0300

    removes using `postcss-flexbugs-fixes` package

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -89,7 +89,6 @@ module.exports = function(webpackEnv) {
           // https://github.com/facebook/create-react-app/issues/2677
           ident: 'postcss',
           plugins: () => [
-            require('postcss-flexbugs-fixes'),
             // Adds PostCSS Normalize as the reset css with default options,
             // so that it honors browserslist config in package.json
             // which in turn let's users customize the target behavior as per their needs.
